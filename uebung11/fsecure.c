3f81a18d8cdbd522fd3199d5192610a512661a9d
GoodTimeclubs <63758794+GoodTimeclubs@users.noreply.github.com>
2025-01-26 01:16:05 +0100

    Implement password input functionality with validation and error handling in fsecure.c

--- uebung11/fsecure.c
+++ uebung11/fsecure.c
@@ -1,14 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "../tools/readtext.c"
 #include "../tools/readint.c"
 
 enum Bool {FALSE,TRUE};
 
 char* get_password(){
-    printf("Bitte geben Sie das Passwort ein");
-    char* pw1 = readtext();
+    int pwlength = 0;
+    int i = 0;
+    char* tpw1;
+    char* pw1;
+    char* pw2;
+    tpw1=getpass("Bitte geben Sie das Passwort ein:");
+
+    printf("tpw1 = %s\n", tpw1);
+
+    while(tpw1[pwlength] != '\0'){
+        pwlength++;
+    }
+
+    pw1 = malloc(pwlength*sizeof(char));
+
+    i = 0;
+    while(tpw1[i] != '\0'){
+        pw1[i] = tpw1[i];
+        i++;
+    }
+    pw1[pwlength] = '\0';
+
+    printf("pw1 = %s\n", pw1);
+
+    pw2=getpass("Bitte geben Sie das Passwort erneut ein:");
+    printf("pw2 = %s\n", pw2);
+
+    i = 0;
+    while(pw1[i] != '\0'){
+        if(pw1[i] != pw2[i]){
+            fputs("Die zwei Passwörter waren nicht Identisch!\n", stderr);
+            exit(10);
+        }
+        i++;
+    }
+    i = 0;
+    while(pw2[i] != '\0'){
+        if(pw1[i] != pw2[i]){
+            fputs("Die zwei Passwörter waren nicht Identisch!\n", stderr);
+            exit(10);
+        }
+        i++;
+    }
+
+
     return pw1;
 }
 
@@ -41,7 +85,13 @@ int main(int argc, char** argv){
     {
         fputs("ERROR - <--encrypt|-e> und <--decrypt|-d> drüfen nicht gelichzeitig verwendet werden!\n", stderr);
         return 1;
+    } else if (!i_encrypt && !i_decrypt) {
+        fputs("ERROR - bitte geben Sie entweden <--encrypt|-e> oder <--decrypt|-d> als Parameter an!\n", stderr);
+        return 1;
     }
+
+
+
     //Input file
     if(argv[1] != argv[i_encrypt] && argv[1] != argv[i_decrypt] && argv[1] != argv[i_output]){
         src = fopen("input.txt", "rb");
@@ -50,24 +100,33 @@ int main(int argc, char** argv){
             fputs("Die Quelldatei konnte nicht geöffnet werden.\n", stderr);
             return 2;
         }
+    } else {
+        fputs("Bitte gib eine Eingabedatei als ersten Parameter an!\n", stderr);
+        return 4;
     }
     
+
+    //Output file
     if(i_output == 0){//wenn keine Output datei angegeben wurde
         fputs("Bitte gib eine Ausgabedatei mit dem Parameter <--output|-o> \"DATEINAME\" an!\n", stderr);
-            return 3;
+        fclose(src);
+        return 5;
     } else {
-        if(i_output+1 < argc){//wenn Output Datei angegeben wurde
+        if(argv[i_output+1] == argv[i_encrypt] && argv[i_output+1] != argv[i_decrypt]){
+            fputs("Es wurde keine Ausgabedatei hinter <--output|-o> angegeben!\n", stderr);
+            fclose(src);
+            return 7;
+        }
+        else if(i_output+1 < argc){//wenn Output Datei angegeben wurde
             dest = fopen(argv[i_output+1],"wb"); //Parameter nach -o oder --output als Ausgabedatei einlesen
             if(dest == NULL)
             {
                 fputs("Die Ausgabedatei konnte nicht geöffnet oder erstellt werden.\n", stderr);
                 fclose(src);
-                return 4;
+                return 6;
             }
-        } else {
-            fputs("Es wurde keine Ausgabedatei hinter <--output|-o> angegeben!\n", stderr);
-            return 5;
         }
+        
     }
 
     //einlesen des Passworts
@@ -85,6 +144,7 @@ int main(int argc, char** argv){
         pw_i ++;
     }
 
+    free(password);
     fclose(src);
     fclose(dest);
 }
